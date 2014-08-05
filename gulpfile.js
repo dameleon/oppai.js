f68a656ccabbb0694238802de455c6fff34b78e9
dameleon <dameleon@gmail.com>
2014-08-06 01:39:32 +0900

    Create gulp tasks, add dist file

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglifyjs');
+var header = require('gulp-header');
+var connect = require('gulp-connect');
+var pkg = require('./package.json');
+
+var BANNER = '/*! <%= name %> / @version:<%= version %> @author:<%= author %> @license:<%= license %> */ \n';
+
+gulp.task('dist', function() {
+    gulp.src('src/**/*.js')
+        .pipe(uglify('oppai.min.js', {
+            outSourceMap: true
+        }))
+        .pipe(header(BANNER, pkg))
+        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('sample/js/'))
+});
+
+gulp.task('develop', function() {
+    gulp.watch('src/**/*.js', ['dist']);
+    connect.server({
+        root: ['sample'],
+        port: 8000,
+        livereload: true
+    });
+});
+
+gulp.task('test', function() {
+    console.log('no test available');
+});
+
+gulp.task('default', ['dist']);
